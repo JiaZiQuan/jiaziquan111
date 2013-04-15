dbb3eb445644a8746cf2c3d60f69edc28222b506
ibokan <ibokan@bokanwensi3-30.local>
2013-04-15 18:36:05 +0800

    Initial Commit

---
+++ 1111/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  1111
+//
+//  Created by ibokan on 13-4-15.
+//  Copyright (c) 2013年 ibokan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ 1111/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  1111
+//
+//  Created by ibokan on 13-4-15.
+//  Copyright (c) 2013年 ibokan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
